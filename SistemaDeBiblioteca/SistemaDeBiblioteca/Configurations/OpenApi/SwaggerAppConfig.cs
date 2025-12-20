fe7ed7993e6b027c0bdc98463a5c1cccd2f11ab5
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-12-19 23:17:30 -0300

    Personalizando a pagina do swagger

---
+++ SistemaDeBiblioteca/SistemaDeBiblioteca/Configurations/OpenApi/SwaggerAppConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace SistemaDeBiblioteca.Configurations.OpenApi
+{
+	public static class SwaggerAppConfig
+	{
+		public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sistema de Biblioteca v1");
+				c.RoutePrefix = string.Empty;
+			});
+
+			return app;
+		}
+	}
+}
